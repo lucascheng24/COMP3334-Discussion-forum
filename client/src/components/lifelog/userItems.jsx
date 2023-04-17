c68b67076e578ceba6ce749fb09d7ba6663d34ee
KelvinLi <limanchungofdragon@gmail.com>
2023-04-18 03:51:24 +0800

    style

--- client/src/components/lifelog/userItems.jsx
+++ client/src/components/lifelog/userItems.jsx
@@ -7,10 +7,11 @@ const UserItems = (props) => {
     <div className="list-group">
       {users.map((user) => (
         <Link
-          className="list-group-item list-group-item-action flex-column align-items-start"
+          className="list-group-item list-group-item-action flex-column align-items-start shadow-sm"
           to={`/personallifelog/${user.username}`}
+          style={{marginTop:"0.5rem", marginBottom:"0.5rem", backgroundColor:"white"}}
         >
-          <div className="d-flex w-100 justify-content-between" key={user.username}>
+          <div className="d-flex w-100 justify-content-between" key={user.username} style={{marginBottom:"-1.5rem"}}>
             <h5 className="mb-1">{user.name}</h5>
           </div>
           <br />
